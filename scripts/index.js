ab7885c68a459e7c79a24c64e037b8c7064c3fb5
Anna Kazaryan <annakaz90@nextmail.ru>
2020-09-01 16:17:40 +0300

    first corrections

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,24 @@
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
-let profileName=document.querySelector('.profile__name');
-let profileJob=document.querySelector('.profile__job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
 let formElement = popup.querySelector('.popup__form');
 let closeButton = popup.querySelector('.popup__close');
-let nameInput=document.querySelector('.popup__name');
-let jobInput=document.querySelector('.popup__job');
-
-//Первоначальные значения в inputах:
-nameInput.value=profileName.textContent;
-jobInput.value=profileJob.textContent;
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
 
 function popupToggle() {
   popup.classList.toggle('popup_opened');
+  if (popup.classList.contains('popup_opened')) {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+  }
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    profileName.textContent=nameInput.value;
-    profileJob.textContent=jobInput.value;
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
     popupToggle();
 }
 
@@ -26,12 +26,5 @@ function formSubmitHandler (evt) {
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 
-function closePopup() {
-  //Отменяем изменения в inputах, чтобы их не было при следующем открытии формы
-  nameInput.value=profileName.textContent;
-  jobInput.value=profileJob.textContent;
-  popupToggle();
-}
-
 editButton.addEventListener('click', popupToggle);
-closeButton.addEventListener('click', closePopup);
+closeButton.addEventListener('click', popupToggle);
